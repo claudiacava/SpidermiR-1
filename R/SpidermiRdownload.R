b03ddce34609347ed4b113a78fe33c93dac77b36
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/SpidermiR@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/SpidermiRdownload.R
@@ -0,0 +1,93 @@
+#' @title Download the network from GeneMania. 
+#' @description
+#'      SpidermiRdownload_net function will download the data 
+#'
+#' @param data The SpidermiRquery_spec_networks output
+#' @examples
+#' org<-SpidermiRquery_species(species)
+#' net_shar_prot<-SpidermiRquery_spec_networks(organismID = org[9,],
+#'                                        network = "SHpd")
+#' out_net<-SpidermiRdownload_net(data=net_shar_prot)
+#' @export
+#' @return Download GeneMania network 
+SpidermiRdownload_net <- function(data){
+    list_d<-list()
+    for(i in 1:length(data)){
+      data<-unlist(data)
+      list_d[[i]]<-read.table(data[i],header = TRUE,stringsAsFactors=FALSE)
+      #print(paste(data[i], " ... reference n. ", i, " of ", length(data), sep = ""))
+    }
+    return (list_d)  
+}
+
+#' @title Download both miRNA target and the gene-drug interaction from PharmacomiR database
+#' @description SpidermiRdownload_pharmacomir will download miRNA Pharmacogenomic data
+#' @param pharmacomir variable
+#' @examples
+#' mir_pharmaco<-SpidermiRdownload_pharmacomir(pharmacomir=pharmacomir)
+#' @export
+#' @import stats
+#' @return a dataframe with gene-drug, and miR-gene associations
+SpidermiRdownload_pharmacomir<-function(pharmacomir){
+  # querying Pharmaco-miR database (Pharmaco-miR validated interaction)
+  pharm_miR <- .url_cache$get("pharmacomir")
+  pharm_miR_db<-read.csv(pharm_miR,header = TRUE,stringsAsFactors=FALSE)
+  pharm_miR_db$miRNA <- as.character(sub("miR-","hsa-miR-", pharm_miR_db$miRNA))
+  pharm_miR_db$miRNA <- as.character(sub("let-","hsa-let-", pharm_miR_db$miRNA))
+  pharm_miR_db$Drug<-as.character(sub("3,3'-","", pharm_miR_db$Drug))
+  pharm_miR_db$Drug<-as.character(sub("5-","", pharm_miR_db$Drug))
+  pmir<-cbind(pharm_miR_db$miRNA,pharm_miR_db$Gene)
+  pmir<-as.data.frame(pmir[!duplicated(pmir), ]) 
+  pmir2<-cbind(pharm_miR_db$Gene,pharm_miR_db$Drug)
+  pmir2<-as.data.frame(pmir2[!duplicated(pmir2), ]) 
+  pmir_c<-rbind(pmir,pmir2)
+  i <- sapply(pmir_c, is.factor)
+  pmir_c[i] <- lapply(pmir_c[i], as.character)
+  return(pmir_c)
+  #SpidermiRvisualize_mirnanet(data=pmir_c)
+}
+
+
+#' @title Download miRNA validated database
+#' @description SpidermiRdownload_miRNAprediction will download miRNA validated target
+#' @param mirna_list miRNA list of interest
+#' @examples
+#' mirna<-c('hsa-miR-567','hsa-miR-566')
+#' list<-SpidermiRdownload_miRNAprediction(mirna_list=mirna)
+#' @export
+#' @import stats
+#' @import org.Hs.eg.db
+#' @import miRNAtap.db
+#' @importFrom miRNAtap getPredictedTargets 
+#' @importFrom org.Hs.eg.db org.Hs.egSYMBOL2EG
+#' @importFrom AnnotationDbi mappedkeys as.list
+#' @return a dataframe with miRNA target validated interactions
+SpidermiRdownload_miRNAprediction<-function(mirna_list){
+  dop=list()
+  for (k in  1:length(mirna_list)){
+    targets <- getPredictedTargets(mirna_list[k],species='hsa', method ='geom')
+    dop[[k]]<-targets
+    names(dop)[[k]]<-mirna_list[k]
+  }
+  x <- org.Hs.egSYMBOL2EG
+  mapped_genes <- mappedkeys(x)
+  xx <- as.list(x[mapped_genes])
+  top <- matrix(0, length(xx), length(dop))
+  rownames(top) <- names(xx)
+  colnames(top)<- names(dop)
+  #j=39
+  #k=1
+  for (j in  1:length(xx)){
+    for (k in  1:length(dop)){
+      if (length(intersect(xx[[j]],rownames(dop[[k]]))!=0)){
+        #print (j)
+        top[j,k]<-names(xx[j])
+      }
+    }  
+  }
+  top[top == 0] <- NA
+  top[apply(top,1,function(x)any(!is.na(x))),]
+  top<-t(top)
+  return(top)
+}
+
